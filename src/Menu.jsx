31dd5cce0915d538707005d123e5cdda3fde198a
costar011 <dnaer456@gmail.com>
2024-02-23 16:39:48 +0900

    nav

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,2 +1,15 @@
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Navigation} from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./styles.css";
+import { useState } from "react";
+
+const navs = [
+    <div>
+        <h2>About</h2>
+        <nav>
+            <a style={{animationDelay: "0.8s"}}>Profile</a>
+        </nav>
+    </div>
+]
