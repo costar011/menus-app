2559967eb804e56ee76725622f97912f60fe2aeb
costar011 <dnaer456@gmail.com>
2024-05-11 12:50:07 +0900

    jsx

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,35 +1,40 @@
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Navigation} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./styles.css";
 import { useState } from "react";
 
 const navs = [
-    <div>
-        <h2>About</h2>
-        <nav>
-            <a style={{animationDelay: "0.8s"}}>Profile</a>
-            <a style={{animationDelay: "0.9s"}}>Skills</a>
-            <a style={{animationDelay: "1s"}}>Education</a>
-        </nav>
-    </div>
-]
+  <div>
+    <h2>About</h2>
+    <nav>
+      <a style={{ animationDelay: "0.8s" }}>Profile</a>
+      <a style={{ animationDelay: "0.9s" }}>Skills</a>
+      <a style={{ animationDelay: "1s" }}>Education</a>
+    </nav>
+  </div>,
+  // more navs
+];
 
 export const Menu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleMenu = () => setIsOpen(!isOpen);
 
-    const openClass = isOpen ? "open" : "";
+  const openClass = isOpen ? "open" : "";
 
-    return (
-        <>
-            <div className={`backgorund ${openClass}`}></div>
-            <button className={`burger ${openClass}`}onclick={toggleMenu}></button>
-            <div className={`menu ${openClass}`}>
-                <Swiper speed={750}></Swiper>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={`backgorund ${openClass}`}></div>
+      <button className={`burger ${openClass}`} onclick={toggleMenu}></button>
+      <div className={`menu ${openClass}`}>
+        <Swiper speed={750} modules={[Navigation]} navigation>
+          {navs.map((nav) => (
+            <SwiperSlide>{nav}</SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
+};
